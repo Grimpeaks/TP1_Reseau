182c2f622bedd51b7c96fa74c674f7acc29acaf1
Grimpeaks <44480134+Grimpeaks@users.noreply.github.com>
2019-09-27 20:58:40 -0400

    Mise en place des 4 projets
    Mise en place des quatres projets: exécutables serveur et client, et libs serveur et client

---
+++ main/exeClient/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Client : Hello world" << std::endl;
+    std::cin.ignore();
+}
---
+++ main/exeServer/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Server : Hello world" << std::endl;
+    std::cin.ignore();
+}
